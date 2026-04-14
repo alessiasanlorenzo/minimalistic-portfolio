33d35cf3ad0a3c34aa322984d44b0e7057a48308
Alessia Sanlorenzo <alessinha@MacBook-Air-di-Alessia.local>
2026-04-14 08:14:21 +0200

    Aggiunto effetto scroll all'header e fix immagini

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const menuIcon = document.querySelector('#menu-icon'); 
-const navLinks = document.querySelector('.nav-links'); 
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
+  navLinks.classList.toggle("active");
+};
+
+window.addEventListener("scroll", function () {
+  // Seleziona il tuo header
+  let header = document.querySelector("header");
+
+  // Aggiunge la classe 'scrolled-nav' se scendi più di 50 pixel
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled-nav");
+  } else {
+    header.classList.remove("scrolled-nav");
+  }
+});
