59439a06f20edf11de9419afd384797383f6aa44
Alessia Vittoria <tuaemail@esempio.comalessiasanlorenzo@libero.it>
2026-04-14 20:34:22 +0200

    Menu a pillola animato con effetto traslazione e opacità

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const menuIcon = document.querySelector("#menu-icon");
 const navLinks = document.querySelector(".nav-links");
+// Aggiungiamo questa riga per selezionare tutti i link dentro la tendina
+const links = document.querySelectorAll(".nav-links a");
 
+// Apre o chiude il menù quando clicchi sull'hamburger
 menuIcon.onclick = () => {
   navLinks.classList.toggle("active");
 };
 
+// NUOVO: Chiude il menù automaticamente quando clicchi su un link
+links.forEach((link) => {
+  link.onclick = () => {
+    navLinks.classList.remove("active");
+  };
+});
+
+// Cambia l'header quando scorri la pagina
 window.addEventListener("scroll", function () {
-  // Seleziona il tuo header
   let header = document.querySelector("header");
 
-  // Aggiunge la classe 'scrolled-nav' se scendi più di 50 pixel
   if (window.scrollY > 50) {
     header.classList.add("scrolled-nav");
   } else {
