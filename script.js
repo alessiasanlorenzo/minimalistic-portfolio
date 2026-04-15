8e317a33888b8d34da19b53ad2a6442482334993
Alessia Vittoria <tuaemail@esempio.comalessiasanlorenzo@libero.it>
2026-04-15 12:32:04 +0200

    Risolto bug animazione menu durante il resize

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const menuIcon = document.querySelector("#menu-icon");
+
 const navLinks = document.querySelector(".nav-links");
+
 // Aggiungiamo questa riga per selezionare tutti i link dentro la tendina
+
 const links = document.querySelectorAll(".nav-links a");
 
 // Apre o chiude il menù quando clicchi sull'hamburger
 menuIcon.onclick = () => {
+  // 1. Applica l'animazione SOLO in questo momento
+  navLinks.style.transition =
+    "opacity 0.4s ease, transform 0.4s ease, visibility 0.4s ease";
+
+  // 2. Apre/Chiude il menù
   navLinks.classList.toggle("active");
 };
 
-// NUOVO: Chiude il menù automaticamente quando clicchi su un link
-links.forEach((link) => {
-  link.onclick = () => {
+// Chiude il menù e blocca l'animazione quando allarghi la finestra
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
     navLinks.classList.remove("active");
-  };
-});
-
-// Cambia l'header quando scorri la pagina
-window.addEventListener("scroll", function () {
-  let header = document.querySelector("header");
-
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled-nav");
-  } else {
-    header.classList.remove("scrolled-nav");
+    navLinks.style.transition = "none"; // Uccide l'animazione durante il ridimensionamento
   }
 });
